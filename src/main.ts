77290dcbfbe7f6ebca615e216b7ca506c98ed8bc
takabosoft <takabosoft@gmail.com>
2025-03-29 11:59:39 +0900

    3D

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@
  * Server: npx live-server docs
  */
 
-import { createNoise2D } from "simplex-noise";
+import { createNoise2D, createNoise3D } from "simplex-noise";
 import { Camera } from "./camera";
 import { Canvas } from "./canvas";
 import { Ray } from "./ray";
 import { Vec3 } from "./vec3";
+import { clamp } from "./mathUtils";
 
 
 
@@ -16,11 +17,11 @@ $(() => {
 });
 
 class FlatCloud {
-    private readonly noise2D = createNoise2D();
+    private readonly noise = createNoise2D();
     private readonly cloudCol = new Vec3(1, 1, 1);
 
     private getNoise(pos: Vec3, scale: number, threshold: number): number {
-        const noise = this.noise2D(pos.x * scale, pos.z * scale);
+        const noise = this.noise(pos.x * scale, pos.z * scale);
         const noise2 = (noise + 1) / 2; // 0-1
         return noise2 >= threshold ? ((noise2 - threshold) * (1 / (1 - threshold))) : 0;
     }
@@ -42,10 +43,58 @@ class FlatCloud {
     }
 }
 
+class VolumeCloud {
+    private readonly noise = createNoise3D();
+    private readonly cloudCol = new Vec3(1, 1, 1);
+
+    private getNoise(pos: Vec3, scale: number, threshold: number): number {
+        const noise = this.noise(pos.x * scale, pos.y * scale, pos.z * scale);
+        const noise2 = (noise + 1) / 2; // 0-1
+        return noise2 >= threshold ? ((noise2 - threshold) * (1 / (1 - threshold))) : 0;
+    }
+
+    private sampleCloudDensity(p: Vec3): number {
+        if (p.y > 100 && p.y < 200) {
+            return this.getNoise(p, 0.01, 0.3);
+        }
+        return 0;
+    }
+
+    private march(r: Ray) {
+        let accumulatedDensity = 0.0; // 密度の累積
+        let transmittance = 1.0; // 透過率
+    
+        const maxSteps = 400;
+        const stepSize = 1;
+        let t = 80;
+        for (let i = 0; i < maxSteps; i++) {
+            const p = r.at(t);
+            const density = this.sampleCloudDensity(p);
+            if (p.y > 200) { break; }
+    
+            const alpha = clamp(density * stepSize * 0.008, 0, 1);
+    
+            accumulatedDensity += alpha * transmittance;
+            transmittance *= (1.0 - alpha);
+    
+            if (transmittance < 0.01) break;
+    
+            t += stepSize;
+        }
+    
+        return clamp(accumulatedDensity, 0, 1);
+    }
+
+    getColor(r: Ray, bg: Vec3): Vec3 {
+        return Vec3.mix(bg, this.cloudCol, this.march(r));
+    }
+}
+
 class App {
     private readonly canvas = new Canvas(640, 480);
     private targetY = 0.5;
-    private flatCloud = new FlatCloud();
+    private cloud = new VolumeCloud();
+    private imageData: ImageData;
 
     constructor() {
         const slider = $(`<input type="range" min="0" max="3" step="0.001" value="${this.targetY}">`).on("input", () => {
@@ -58,14 +107,14 @@ class App {
             this.canvas.canvas,
             slider,
         );
+        this.imageData = this.canvas.ctx.createImageData(this.canvas.width, this.canvas.height);
         this.render();
     }
 
     render() {
         const canvasWidth = this.canvas.width;
         const canvasHeight = this.canvas.height;
-        const imageData = this.canvas.ctx.createImageData(canvasWidth, canvasHeight);
-        const data = imageData.data;
+        const data = this.imageData.data;
 
         const aspectRatio = canvasWidth / canvasHeight;
         const camera = new Camera(new Vec3(0, 0, 0), new Vec3(0, this.targetY, -1), new Vec3(0, 1, 0), 70, aspectRatio);
@@ -82,7 +131,7 @@ class App {
                 data[idx + 3] = 255;
             }
         }
-        this.canvas.ctx.putImageData(imageData, 0, 0);
+        this.canvas.ctx.putImageData(this.imageData, 0, 0);
     }
 
     private getBackgroundColor(r: Ray): Vec3 {
@@ -95,7 +144,7 @@ class App {
 
     private getColor(r: Ray): Vec3 {
         let col = this.getBackgroundColor(r);
-        col = this.flatCloud.getColor(r, col);
+        col = this.cloud.getColor(r, col);
         return col;
     }
 }
